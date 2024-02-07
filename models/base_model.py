632c4f471b0edd96eb847ee73e528ed89dd05621
O-G-W-A-L <ogwalja04@gmail.com>
2024-02-06 16:02:44 -0800

    store first

--- models/base_model.py
+++ models/base_model.py
@@ -4,17 +4,30 @@ contains class base models
 """
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
-    def __init__(self):
-        """Initialize Base model  instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """initialize base model instance"""
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "update_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        models.storage.new(self)
         
     def save(self):
         """updates the attribute 'updated_at' with the current datetime """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ """
