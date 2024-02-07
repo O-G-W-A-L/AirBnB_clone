e550377361424ccf68dc49f3a6ce209434361a7d
O-G-W-A-L <ogwalja04@gmail.com>
2024-02-06 23:58:08 -0800

    Console 0.1

--- console.py
+++ console.py
@@ -3,11 +3,19 @@
 """This is the console base for the unit """
 
 import cmd
+import shlex
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Class for the command interpreter"""
 
     prompt = "(hbnb)"
+    valid_classes = ["BaseModel"]
+
+    def emptylibe(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
     def do_quit(self, arg):
         """Close program and saves safely data """
@@ -18,9 +26,100 @@ class HBNBCommand(cmd.Cmd):
         print("Quit command to exit")
         
     def do_EOF(self, arg):
-        """EOF command to exit the program."""
+        """End Of File command to exit the program."""
         print()
         return True
+
+    def do_create(self, arg):
+        """create a new instance of BaseModel and saves it to JSON file"""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instnce id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                print(objects[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """deletes an instance based on class name and id"""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects =storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """prints a string representation of all instances"""
+        objects = storage.all()
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == commands[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """updates an instance by adding or updating an attribute"""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn'y exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key not in objects:
+                print("** no instance found **")
+            elif len(commands) < 3:
+                print("** attribute name missing **")
+            elif len(commands) < 4:
+                print("** value missing **")
+            else:
+                obj = objects[key]
+                attr_name = commands[2]
+                attr_value = commands[3]
+
+                try:
+                    attr_value = eval(attr_value)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_value)
+                obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
